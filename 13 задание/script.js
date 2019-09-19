346383bd26df55fed2ee80785c816c9742058c81
nurgi17 <usen.nurgisa@gmail.com>
2019-09-19 16:40:32 +0600

    Slider & Counter

--- 13 задание/script.js
+++ 13 задание/script.js
@@ -1,50 +1,93 @@
-//AJAX
+// //AJAX
 'use strict'
+// let inputRub = document.getElementById('rub'),
+//     inputUsd = document.getElementById('usd');
+
+// function aajax() {
+//     let request = new XMLHttpRequest();
+
+//     //------------method, url,          async,         login, password
+//     request.open('GET', 'current.json');//default->true
+//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//     request.send();
+//     return request;
+// }
+// function calc(request, currency) {
+//     if (request.readyState === 4 && request.status === 200) {
+//         let data = JSON.parse(request.response);
+//         if (currency == 'rub') {
+//             inputUsd.value = inputRub.value / data.usd;
+//         } else if (currency == 'usd') {
+//             inputRub.value = inputUsd.value * data.usd;
+//         }
+//     } else {
+//         input.value = "Smth was wrong!";
+//     }
+// }
+
+
+// inputRub.addEventListener('input', () => {
+//     let request = aajax();
+//     //status -> 404, 500 and so on 
+//     //statusText -> OK or NOT FOUND
+//     //responseText / response
+//     //readyState
+//     request.addEventListener('readystatechange', function () {// load
+//         let cur = 'rub';
+//         calc(request, cur);
+//     });
+// });
+
+// inputUsd.addEventListener('input', () => {
+//     let request = aajax();
+//     request.addEventListener('readystatechange', function () {
+//         let cur = 'usd';
+//         calc(request, cur);
+//     });
+// });
+
+
+
+//With ES6 Promises 
 let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
-function aajax() {
-    let request = new XMLHttpRequest();
+function catchData() {
+    return new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        request.open('GET', 'current.json');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        request.send();
 
-    //------------method, url,          async,         login, password
-    request.open('GET', 'current.json');//default->true
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
-    return request;
+        request.onload = function () {
+            if (request.readyState === 4) {
+                if (request.status == 200) {
+                    resolve(this.response);
+                }
+                else {
+                    reject();
+                }
+            }
+        };
+    });
 }
-function calc(request, currency) {
-    if (request.readyState === 4 && request.status === 200) {
-        let data = JSON.parse(request.response);
-        if (currency == 'rub') {
-            inputUsd.value = inputRub.value / data.usd;
-        } else if (currency == 'usd') {
-            inputRub.value = inputUsd.value * data.usd;
-        }
-    } else {
-        input.value = "Smth was wrong!";
-    }
-}
-
 
 inputRub.addEventListener('input', () => {
-
-    let request = aajax();
-    //status -> 404, 500 and so on 
-    //statusText -> OK or NOT FOUND
-    //responseText / response
-    //readyState
-    request.addEventListener('readystatechange', function () {// load
-        let cur = 'rub';
-        calc(request, cur);
-    });
+    catchData()
+        .then(response => {
+            console.log(response);
+            let data = JSON.parse(response);
+            inputUsd.value = inputRub.value / data.usd;
+        })
+        .then(() => console.log(5000))
+        .catch(() => inputUsd.value = "Smth was wrong!!!");
 });
 
 inputUsd.addEventListener('input', () => {
-    let request = aajax();
-    request.addEventListener('readystatechange', function () {
-        let cur = 'usd';
-        calc(request, cur);
-    });
+    catchData()
+        .then(response => {
+            let data = JSON.parse(response);
+            inputRub.value = inputUsd.value * data.usd;
+        })
+        .catch(() => inputUsd.value = "Smth was wrong!!!");
 });
-
-
--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', function (e) {
     'use strict';
+    //Tabs –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent'),
@@ -34,7 +35,7 @@ window.addEventListener('DOMContentLoaded', function (e) {
     });
     //——————————————————————————————————————————————————————————————————————————————
 
-    //Timer
+    //Timer ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
     let deadline = '2019-09-20';
 
     function getTimeRemaining(endtime) {
@@ -85,9 +86,9 @@ window.addEventListener('DOMContentLoaded', function (e) {
         }
     }
     setClock('timer', deadline);
+    //Timer END ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-
-    // Modal window
+    //Modal window ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
     let more = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
@@ -109,9 +110,11 @@ window.addEventListener('DOMContentLoaded', function (e) {
     for (let i = 0; i < desBtn.length; i++) {
         desBtn[i].addEventListener('click', showModal);
     }
+    //Modal Window END ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 
 
+    //AJAX with promises XMLHttpRequest, checking method POST ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
     let message = {
         loading: 'Loading...',
         success: 'Thank you',
@@ -123,52 +126,197 @@ window.addEventListener('DOMContentLoaded', function (e) {
         contact = document.getElementById('form'),
         inputs = contact.getElementsByTagName('input');
 
-    function requestServer() {
+    //     function requestServer() {
 
-        let request = new XMLHttpRequest();
-        request.open('POST', '../server.php');
-        //request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-        let formData = new FormData(form);
+    //         let request = new XMLHttpRequest();
+    //         request.open('POST', '../server.php');
+    //         //request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    //         request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    //         let formData = new FormData(form);
 
-        let obj = {};
+    //         let obj = {};
 
-        formData.forEach(function (value, key) {
-            obj[key] = value;
-        });
-        let json = JSON.stringify(obj);
-        request.send(json);
+    //         formData.forEach(function (value, key) {
+    //             obj[key] = value;
+    //         });
+    //         let json = JSON.stringify(obj);
+    //         request.send(json);
 
-        request.addEventListener('readystatechange', function () {
-            if (request.readyState < 4) {
-                statusMessage.innerHTML = message.loading;
-            } else if (request.readyState === 4 && request.status == 200) {
-                statusMessage.innerHTML = message.success;
-            } else {
-                statusMessage.innerHTML = message.failure;
+    //         request.addEventListener('readystatechange', function () {
+    //             if (request.readyState < 4) {
+    //                 statusMessage.innerHTML = message.loading;
+    //             } else if (request.readyState === 4 && request.status == 200) {
+    //                 statusMessage.innerHTML = message.success;
+    //             } else {
+    //                 statusMessage.innerHTML = message.failure;
+    //             }
+    //         });
+    //     }
+
+
+    //     statusMessage.classList.add('status');
+
+    //     form.addEventListener('submit', function (e) {
+    //         e.preventDefault();
+    //         form.appendChild(statusMessage);
+    //         requestServer();
+    //         for (let i = 0; i < input.length; i++) {
+    //             input[i].value = '';
+    //         }
+    //     });
+
+    //     contact.addEventListener('submit', function (e) {
+    //         e.preventDefault();
+    //         contact.appendChild(statusMessage);
+    //         requestServer();
+
+    //         for (let i = 0; i < inputs.length; i++) {
+    //             inputs[i].value = '';
+    //         }
+    //     });
+
+    // });
+
+
+
+    function requestServer(elem) {
+        elem.addEventListener('submit', (e) => {
+            e.preventDefault();
+            elem.appendChild(statusMessage);
+            let formData = new FormData(form);
+
+            function postData(data) {
+                return new Promise((resolve, reject) => {
+
+                    let request = new XMLHttpRequest();
+                    request.open('POST', '../server.php');
+                    //request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+                    request.onreadystatechange = function () {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4) {
+                            if (request.status == 200 && request.status < 300) {
+                                resolve();
+                            }
+                            else {
+                                reject();
+                            }
+                        }
+                    };
+                    request.send(data);
+                });
             }
+
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+            }
+            postData(formData)
+                .then(() => statusMessage.innerHTML = message.loading)
+                .then(() => {
+                    statusMessage.innerHTML = '';
+                })
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .then(clearInput);
         });
+
+    }
+    requestServer(form);
+    requestServer(contact);
+
+    //AJAX with promises XMLHttpRequest, checking method POST END––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+
+    //Slider ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach((item) => item.style.display = 'none');
+        // for (let i = 0; i < slides.length; i++) {
+        //     slides[i].style.display = 'none';
+        // }
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
     }
 
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
 
-    statusMessage.classList.add('status');
+    prev.addEventListener('click', function (e) {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', function (e) {
+        plusSlides(1);
+    });
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        form.appendChild(statusMessage);
-        requestServer();
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = '';
+    dotsWrap.addEventListener('click', function (e) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (e.target.classList.contains('dot') && e.target == dots[i - 1]) {
+                currentSlide(i);
+            }
         }
     });
 
-    contact.addEventListener('submit', function (e) {
-        e.preventDefault();
-        contact.appendChild(statusMessage);
-        requestServer();
+    //Slider END ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-        for (let i = 0; i < inputs.length; i++) {
-            inputs[i].value = '';
+
+    //Calculator ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0, daysSum = 0, total = 0;
+
+    totalValue.innerHTML = 0;
+
+    persons.addEventListener('change', function (e) {
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+    restDays.addEventListener('change', function (e) {
+        daysSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+        if (persons.value == '' || restDays.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function (e) {
+        let a = total,
+            b = this.options[this.selectedIndex].value;
+        if (persons.value == '' || restDays.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = a * b;
         }
     });
 
