8c98e9b131573a19293364629fd616f15fb4403a
nurgi17 <usen.nurgisa@gmail.com>
2020-07-09 15:07:50 +0600

    Finished the giphy app on native js

--- JS2(onemonth)/2 week/GiphySearch/javascript/main.js
+++ JS2(onemonth)/2 week/GiphySearch/javascript/main.js
@@ -2,40 +2,40 @@
 var button = document.querySelector('.js-go')
 button.addEventListener('click', () => {
     var input = document.querySelector('input').value
-    pushToDOM(input)
+    search(input)
 })
 
 var input1 = document.querySelector('.js-userinput')
 input1.addEventListener('keyup', (e) => {
     var input = document.querySelector('input').value
     if (e.which === 13) {
-      pushToDOM(input)
+      search(input)
     }
 })
 
-var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+function search (req) {
+  var url = `https://api.giphy.com/v1/gifs/search?q=${req}&api_key=dc6zaTOxFJmzC`
 
-// AJAX Request
-var GiphyAJAXCall = new XMLHttpRequest();
-GiphyAJAXCall.open( 'GET', url );
-GiphyAJAXCall.send();
-
-GiphyAJAXCall.addEventListener('load', (e) => {
-
-  var data = e.target.response;
-  pushToDOM(data)
-
-})
-
+  // AJAX Request
+  var GiphyAJAXCall = new XMLHttpRequest()
+  GiphyAJAXCall.open( 'GET', url )
+  GiphyAJAXCall.send()
+  
+  GiphyAJAXCall.addEventListener('load', (e) => {
+  
+    var data = e.target.response
+    pushToDOM(data)
+  
+  })
+}
 
 function pushToDOM (input) {
     var response = JSON.parse(input)
     var imageUrls = response.data
+    var container = document.querySelector('.js-container')
+    container.innerHTML = ''
     imageUrls.forEach((image) => {
       var src = image.images.fixed_height.url
-      console.log(src)
-      var container = document.querySelector(".js-container")
-      container.innerHTML += "<img src=\"" + src + "\" class=\"container-image\">"
+      container.innerHTML += '<img src=\'' + src + '\' class=\'container-image\'>'
     })
-
 }
